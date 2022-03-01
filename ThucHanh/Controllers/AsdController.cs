3b32c602d4dc1ead558f0078d699d54bbc53d293
lamducthao <lamducthao9x@gmail.com>
2022-03-01 08:56:18 +0700

    Add project files.

---
+++ ThucHanh/Controllers/AsdController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThucHanh.Controllers
+{
+    public class AsdController : Controller
+    {
+        // GET: Asd
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Demo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Demo(string Masinhvien, string Hoten)
+        {
+            ViewBag.message = Masinhvien + " " + Hoten;
+            return View();
+        }
+    }
+}
